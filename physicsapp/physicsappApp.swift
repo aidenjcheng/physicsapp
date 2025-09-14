6842035fd0a03922ced7f9315cacd39c90c625c4
aidenjcheng <aidenjcheng12@gmail.com>
2025-09-13 23:14:23 -0400

    Initial Commit

---
+++ physicsapp/physicsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  physicsappApp.swift
+//  physicsapp
+//
+//  Created by Michelle on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct physicsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
